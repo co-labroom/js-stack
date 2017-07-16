1f9183bf4b63381c06617c4121f68ce66c946a3e
unnKoel <unnkoel@gmail.com>
2017-07-16 22:02:11 +0800

    async react-redux

---
+++ src/client/component/page/home.js
@@ -0,0 +1,10 @@
+/**
+ * Created by unnKoel on 2017/7/16.
+ */
+
+// @flow
+import React from 'react'
+
+const HomePage = () => <p>Home</p>
+
+export default HomePage
--- src/client/index.js
+++ src/client/index.js
@@ -8,26 +8,32 @@ import 'babel-polyfill'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
-import { Provider } from 'react-redux'
-import { createStore, combineReducers } from 'redux'
+import {AppContainer} from 'react-hot-loader'
+import {Provider} from 'react-redux'
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import {BrowserRouter} from 'react-router-dom'
 
 import App from './app'
 import helloReducer from './reducer/hello'
-import { APP_CONTAINER_SELECTOR } from '../shared/config'
-import { isProd } from '../shared/util'
+import {APP_CONTAINER_SELECTOR} from '../shared/config'
+import {isProd} from '../shared/util'
 
-const store = createStore(combineReducers({ hello: helloReducer }),
-  // eslint-disable-next-line no-underscore-dangle
-  isProd ? undefined : window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+
+const store = createStore(combineReducers({hello: helloReducer}),
+  composeEnhancers(applyMiddleware(thunkMiddleware)))
 
 const rootEl = document.querySelector(APP_CONTAINER_SELECTOR)
 
 const wrapApp = (AppComponent, reduxStore) =>
   <Provider store={reduxStore}>
-    <AppContainer>
-      <AppComponent />
-    </AppContainer>
+    <BrowserRouter>
+      <AppContainer>
+        <AppComponent />
+      </AppContainer>
+    </BrowserRouter>
   </Provider>
 
 ReactDOM.render(wrapApp(App, store), rootEl)
