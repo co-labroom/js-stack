3c0993c71476950bc9bae9e854d8e2f8698e8262
unnKoel <unnkoel@gmail.com>
2017-07-16 23:07:21 +0800

    ssr

--- src/server/index.js
+++ src/server/index.js
@@ -6,23 +6,17 @@
 import compression from 'compression'
 import express from 'express'
 
-import { APP_NAME, STATIC_PATH, WEB_PORT } from '../shared/config'
+import routing from './routing'
+import { WEB_PORT, STATIC_PATH } from '../shared/config'
 import { isProd } from '../shared/util'
-import renderApp from './render-app'
-import { helloEndPointRoute } from '../shared/routes'
 
 const app = express()
+
 app.use(compression())
 app.use(STATIC_PATH, express.static('dist'))
 app.use(STATIC_PATH, express.static('public'))
 
-app.get('/', (req, res) => {
-  res.send(renderApp(APP_NAME))
-})
-
-app.get(helloEndPointRoute(), (req, res) => {
-  res.json({ serverMessage: `Hello from the server! (received ${req.params.num})` })
-})
+routing(app)
 
 app.listen(WEB_PORT, () => {
   // eslint-disable-next-line no-console
--- src/server/render-app.js
+++ src/server/render-app.js
@@ -3,21 +3,42 @@
  */
 
 // @flow
-import { APP_CONTAINER_CLASS, STATIC_PATH, WDS_PORT } from '../shared/config'
-import { isProd } from '../shared/util'
 
-const renderApp = (title: string) =>
-  `<!doctype html>
-  <html>
-    <head>
-      <title>${title}</title>
-      <link rel="stylesheet" href="${STATIC_PATH}/css/style.css">
-    </head>
-    <body>
-      <div class="${APP_CONTAINER_CLASS}"></div>
-      <script src="${isProd ? STATIC_PATH : `http://localhost:${WDS_PORT}/dist`}/js/bundle.js"></script>
-    </body>
-  </html>
-  `
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import {Provider} from 'react-redux'
+import {StaticRouter} from 'react-router'
+
+import initStore from './init-store'
+import App from './../shared/app'
+import {APP_CONTAINER_CLASS, STATIC_PATH, WDS_PORT} from '../shared/config'
+import {isProd} from '../shared/util'
+
+const renderApp = (location: string, plainPartialState: ?Object, routerContext: ?Object = {}) => {
+  const store = initStore(plainPartialState)
+  const appHtml = ReactDOMServer.renderToString(
+    <Provider store={store}>
+      <StaticRouter location={location} context={routerContext}>
+        <App />
+      </StaticRouter>
+    </Provider>)
+
+  return (
+    `<!doctype html>
+    <html>
+      <head>
+        <title>FIX ME</title>
+        <link rel="stylesheet" href="${STATIC_PATH}/css/style.css">
+      </head>
+      <body>
+        <div class="${APP_CONTAINER_CLASS}">${appHtml}</div>
+        <script>
+          window.__PRELOADED_STATE__ = ${JSON.stringify(store.getState())}
+        </script>
+        <script src="${isProd ? STATIC_PATH : `http://localhost:${WDS_PORT}/dist`}/js/bundle.js"></script>
+      </body>
+    </html>`
+  )
+}
 
 export default renderApp
